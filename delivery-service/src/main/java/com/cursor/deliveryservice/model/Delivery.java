91c91bb99f9965c0a2ad77c53e1c1e9d0801c929
Ronashovkova <rona_t@icloud.com>
2020-03-25 20:40:59 +0200

    added microservices

---
+++ delivery-service/src/main/java/com/cursor/deliveryservice/model/Delivery.java
@@ -0,0 +1,16 @@
+package com.cursor.deliveryservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Delivery {
+    private Long id;
+    private String address;
+    private LocalDateTime time;
+}
